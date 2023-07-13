90f4142880623f6c9175e57b68a67f70b92bccd5
menhunging <menhunging@gmail.com>
2023-07-13 15:16:15 +0300

    fix order

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
   if ($(".checkout-list").length > 0) {
     $(".btn-change").on("click", function () {
+      $(".checkout-item").removeClass("opened");
+      $(".checkout-item__body").stop().slideUp();
+
       $(this)
         .parents(".checkout-item")
+        .addClass("opened")
         .find(".checkout-item__body")
+        .stop()
         .slideDown();
     });
 
@@ -11,18 +16,26 @@ $(document).ready(function () {
       let parents = $(this).parents(".checkout-item");
 
       openCheckItem(parents.prev(".checkout-item"));
-      parents.find(".checkout-item__body").slideUp();
+      parents
+        .removeClass("opened")
+        .find(".checkout-item__body")
+        .stop()
+        .slideUp();
     });
 
     $(".btnCheck-next").on("click", function () {
       let parents = $(this).parents(".checkout-item");
 
       openCheckItem(parents.next(".checkout-item"));
-      parents.find(".checkout-item__body").slideUp();
+      parents
+        .removeClass("opened")
+        .find(".checkout-item__body")
+        .stop()
+        .slideUp();
     });
 
     function openCheckItem(block) {
-      block.find(".checkout-item__body").slideDown();
+      block.addClass("opened").find(".checkout-item__body").stop().slideDown();
     }
   }
 });
